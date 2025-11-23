8aeb383362c73f6768980738c5a68d834d3909de
L O U I S <luis_henano@hotmail.com>
2025-11-23 15:00:07 +0700

    feat: configure dynamic port retrieval from environment variables

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || 3000;
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -20,6 +23,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
